fa5d97daeeab8fd6cc951e17792b2b52b70af1a4
LAG1819 <luisa.gloger@gmx.de>
2024-04-29 21:35:26 +0200

    Added Docker Setup in Shell Script. If Docker is not installed the System will download it directly. If Docker already is installed the docker container will be setup (if it not already exists) and the docker container will be started.

--- run_setup.sh
+++ run_setup.sh
@@ -1,35 +1,76 @@
 #!/bin/bash
 # Detect the operating system
 OS=$(uname -s)
+# Define the image name
+IMAGE_NAME="jupyter/pyspark-notebook"
+
+#Define the container name
+CONTAINER_NAME_OR_ID = jupyter/pyspark-notebook
 
 # Check if Docker is installed
 if command -v docker &> /dev/null; then
     echo "Docker is installed"
+
+    # Check if the docker image exists
+    if docker images --format "{{.Repository}}:{{.Tag}}" | grep -q "$IMAGE_NAME"; then
+        echo "$IMAGE_NAME has been pulled"
+    else
+        echo "$IMAGE_NAME has not been pulled. Image will be pulled."
+        docker pull jupyter/pyspark-notebook
+    fi
+
+   # Check if the container exists
+    if docker ps -a -q --filter "name=$CONTAINER_NAME_OR_ID" 1>/dev/null; then
+        # Check if the container is running
+        if docker ps -q --filter "name=$CONTAINER_NAME_OR_ID" 1>/dev/null; then
+            echo "Container $CONTAINER_NAME_OR_ID is already running."
+        else
+            # Start the container if it exists but is not running
+            docker start $CONTAINER_NAME_OR_ID
+            echo "Container $CONTAINER_NAME_OR_ID started."
+        fi
+    else
+        echo "Container $CONTAINER_NAME_OR_ID does not exist. Container will be set up."
+        docker run -p 10001:8888 \
+        -v src:/home/jovyan/work/ \
+        jupyter/pyspark-notebook
+
+    fi
+# if docker is not installed the corresponding docker executable will be installed for the corresponding operating system.
 else
     echo "Docker is not installed. Please install Docker."
     # Check if the operating system is macOS
     if [[ "$OS" =~ Darwin ]]; then
         echo "macOS detected"
-        xdg-open https://www.docker.com/products/docker-desktop/
+        curl -o docker.dmg https://desktop.docker.com/mac/main/arm64/Docker.dmg?utm_source=docker&utm_medium=webreferral&utm_campaign=dd-smartbutton&utm_location=module
+        echo "docker downloaded"
+        hdiutil mount docker.dmg  # Mount the disk image
+        # Optionally, copy the application to the Applications folder
+        cp -R /Volumes/Application.app /Applications/
     # Check if the operating system is Linux
     elif [[ "$OS" =~ Linux ]]; then
         echo "Linux detected"
-        xdg-open https://www.docker.com/products/docker-desktop/
+        curl -o docker.deb https://docs.docker.com/desktop/linux/install/
+        echo "docker downloaded"
+        sudo dpkg -i docker.deb
     # Check if the operating system is Windows
     elif [[ "$OS" =~ MINGW64_NT ]]; then
         echo "Windows detected"
+        # Run PowerShell command as administrator
+        powershell -Command "Start-Process wsl -ArgumentList '--install' -Verb RunAs"
+        echo "A WSL (Windos Subsystem for Linux) has been downloaded. Please double-click the downloaded installer to start the wsl. You need to set a Username and Password."
+
         #check if wsl2 is installed as it is needed to work with docker
-        start https://www.docker.com/products/docker-desktop/
+        curl -o docker.exe https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe?utm_source=docker&utm_medium=webreferral&utm_campaign=dd-smartbutton&utm_location=module
+        echo "Docker Desktop installer downloaded. Please double-click the downloaded installer docker.exe to install Docker Desktop."
+
+        shutdown /r /t 0
+
     # If the operating system is none of the above
     else
         echo "Unsupported operating system: $OS"
     fi
 fi
 
-# Run Docker with prefabricated image
-docker pull jupyter/pyspark-notebook
 
-docker run -p 10001:8888 \
--v src:/home/jovyan/work/ \
-jupyter/pyspark-notebook
 
