50634a7f6998a325828a5fe29604be70c8948c5a
LAG1819 <44361011+LAG1819@users.noreply.github.com>
2024-05-03 10:11:41 +0200

    Added mounted sourcecode and data folders to jupyter lab and active removal of docker container after stopping container.

--- run_setup.sh
+++ run_setup.sh
@@ -4,33 +4,41 @@ OS=$(uname -s)
 # Define the image name
 IMAGE_NAME="jupyter/pyspark-notebook"
 CONTAINER_NAME_OR_ID="pyspark_workshop"
+SCRIPT_DIR=$(pwd -W)
+SRC_DIR_SRC=$SCRIPT_DIR/src
+SRC_DIR_DATA=$SCRIPT_DIR/data
 
-docker_conainer_creation () {
-    docker run -p 10001:8888 \
-    -v src:/home/jovyan/work \
+docker_container_creation () {
+    docker run --rm -p 10001:8888 \
+    -v $SRC_DIR_SRC:/home/jovyan/src \
+    -v $SRC_DIR_DATA:/home/jovyan/data \
     --name "$1" \
     "$2"
 }
 
-
+#-v "$SCRIPT_DIR/src:/mnt/src" \
 # Check if Docker is installed
 if command -v docker &> /dev/null; then
     echo "Docker is installed"
 
     # Check if the docker image exists
-    if docker ps --filter "name=$IMAGE_NAME" --format '{{.Names}}' | grep -q "$IMAGE_NAME"; then
+    if docker images | grep -q $IMAGE_NAME; then
         echo "$IMAGE_NAME has been pulled. Docker container will be newly setup."
         
-        # Remove the existing container
-        docker rm "$CONTAINER_NAME_OR_ID"
+        # Remove the existing container if it still exists 
+        if docker ps -a | grep "$CONTAINER_NAME_OR_ID"; then
+            container_id=$(docker ps -aqf "name=pyspark_workshop" -a)
+            docker stop "$CONTAINER_NAME_OR_ID"
+            docker rm -f $container_id
+        fi
         
         # Create the new container
-        docker_conainer_creation "$CONTAINER_NAME_OR_ID" "$IMAGE_NAME"
+        docker_container_creation "$CONTAINER_NAME_OR_ID" "$IMAGE_NAME"
     else
         echo "$IMAGE_NAME has not been pulled. Image will be pulled and Container $CONTAINER_NAME_OR_ID will be set up"
         
         docker pull "$IMAGE_NAME"
-        docker_conainer_creation "$CONTAINER_NAME_OR_ID" "$IMAGE_NAME"
+        docker_container_creation "$CONTAINER_NAME_OR_ID" "$IMAGE_NAME"
     fi
     
 # if docker is not installed the corresponding docker executable will be installed for the corresponding operating system.
@@ -55,6 +63,13 @@ else
         echo "Windows detected"
         # Run PowerShell command as administrator
         powershell -Command "Start-Process wsl -ArgumentList '--install' -Verb RunAs"
+        # Attempt to update WSL to version 2 as it is the recommended one. 
+        if wsl.exe --update &> /dev/null; then
+            echo "WSL 2 is installed."
+        else
+            echo "WSL 2 is not installed or an error occurred. Please install manually with the command: wsl.exe --update"
+            # Here you can handle the case where WSL 2 is not installed or an error occurred
+        fi
         echo "A WSL (Windos Subsystem for Linux) has been downloaded. Please double-click the downloaded installer to start the wsl. You need to set a Username and Password."
 
         #check if wsl2 is installed as it is needed to work with docker
